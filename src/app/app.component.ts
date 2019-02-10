48014974d1fc37044a2e540dfe2ec625c58cc3ed
Pavani <pavani.mme@gmail.com>
2019-02-10 21:01:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Http2Server } from 'http2';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  @Input() noOfImages = 5;
+  images = [];
+  constructor( private http: HttpClient){
+
+  }
+  clickEvent(){
+    const url = "https://api.pexels.com/v1/search?query=nature&page=1&per_page="+ this.noOfImages;
+    this.http.get(url, {
+      headers : new HttpHeaders({
+        "Authorization" : "563492ad6f9170000100000195a054d4a56547aab275017c0c80e61b",
+      })
+    }).subscribe(
+      response => {
+        const data = response;
+        console.log(data);
+        this.images = data['photos'];
+      },
+      error => {
+
+      }
+    )
+  }
 }
